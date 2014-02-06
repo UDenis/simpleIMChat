89828c31e52568e5996dcb97c884b4300c7e388c
UDenis <denis@ucluster.ru>
2014-02-06 23:15:04 +0600

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,17 @@
+(function() {
+
+  var chatViewModel = function() {
+
+    this.addMessage = function() {
+      // body...
+    };
+
+    this.formatMsg = function(msg) {
+      // body...
+    };
+
+    this.
+
+  };
+
+})()
